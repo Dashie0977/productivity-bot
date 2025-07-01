ae3470ad3b9e28056319e1c78e7a50e25880a62a
Dashie <dashie.r0wella@gmail.com>
2025-07-01 17:02:25 +0300

    added two commands: hello and echo

---
+++ commands/echo.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('echo')
+        .setDescription('echoes the user.')
+        .addStringOption(option => 
+            option.setName('message')
+            .setDescription('The message to echo')
+            .setRequired(true)),
+            async execute(interaction) {
+                const message = interaction.options.getString('message');
+                await interaction.reply(message);
+            }
+}
---
+++ commands/hello.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Greets the user.'),
+    async execute(interaction) {
+    await interaction.reply(`Hello ${interaction.user.username}! I am your assistant Friday!`);
+    },
+};
